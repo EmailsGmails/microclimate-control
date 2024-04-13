60b671bfcaff2f1d960f89f2f832c313c941a921
Emīls Goško <emils.gosko@gmail.com>
2024-04-13 21:57:04 +0300

    Initialize Django project with Postgres

---
+++ microclimate_control_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MicroclimateControlAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'microclimate_control_app'
